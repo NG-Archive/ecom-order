23d14624313d0d25d4d44e1849fea5bcfc0d89fe
Dasom Kim <69332203+ttony-kim@users.noreply.github.com>
2026-03-22 20:07:47 +0900

    Fix member의 상태가 NORMAL이 아닐 경우 exception 수정

--- src/main/java/site/ng_archive/ecom_order/domain/OrderService.java
+++ src/main/java/site/ng_archive/ecom_order/domain/OrderService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import site.ng_archive.ecom_common.auth.UserContext;
-import site.ng_archive.ecom_common.handler.EntityNotFoundException;
+import site.ng_archive.ecom_common.auth.exception.ForbiddenException;
 import site.ng_archive.ecom_order.domain.dto.CreateOrderCommand;
 import site.ng_archive.ecom_order.domain.dto.OrderListResponse;
 import site.ng_archive.ecom_order.domain.dto.OrderResponse;
@@ -21,7 +21,7 @@ public class OrderService {
     public Flux<OrderListResponse> readAllOrders(UserContext user, long offset, int size) {
         return memberRequester.getMember(user.id())
             .filter(member -> "NORMAL".equals(member.status()))
-            .switchIfEmpty(Mono.defer(() -> Mono.error(new EntityNotFoundException("member.status.invalid"))))
+            .switchIfEmpty(Mono.defer(() -> Mono.error(new ForbiddenException("member.status.invalid"))))
             .flatMapMany(member -> orderRepository.findByAll(offset, size, member.id()))
             .map(OrderListResponse::from);
     }
